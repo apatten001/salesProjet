686ce37c2f4e06d9773a2d02f824f9621a9ea812
Arnold Patten <apatten001@Arnolds-Mac-mini.local>
2022-02-26 10:23:52 -0500

    Add a sales person component and class

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+
+  // create array of salesperson objects
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Arnold","Patten", "arnold.hcl.com", 85000),
+    new SalesPerson("Rob","Pern", "Rob.hcl.com", 70000),
+    new SalesPerson("Aj","Pote", "aj.hcl.com", 75000),
+    new SalesPerson("Chris","Peter", "chris.hcl.com", 80000) 
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
